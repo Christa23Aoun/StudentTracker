2b4ff2963c5c6101bc5bf8f8cd2b93dc26bb9d17
Christa23Aoun <153355873+Christa23Aoun@users.noreply.github.com>
2025-11-03 23:44:37 +0200

    faculties button : departments

--- StudentTracker/Models/DepartmentView.cs
+++ StudentTracker/Models/DepartmentView.cs
@@ -5,5 +5,7 @@
         public int DepartmentID { get; set; }
         public string DepartmentName { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; }
+        public string? Description { get; set; }
+        public bool IsActive { get; set; }
     }
 }
